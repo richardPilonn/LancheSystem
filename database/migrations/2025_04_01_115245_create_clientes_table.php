a3f7e6565ddbdbc1c9ee9b6e72fc3e9b72f99fa8
richardPilonn <richard.pilonzera@gmail.com>
2025-04-01 09:54:38 -0300

    migrations cliente e produto

---
+++ database/migrations/2025_04_01_115245_create_clientes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100)->nullable(false);
+            $table->string('endereco', 100)->nullable(false);
+            $table->string('telefone', 80)->nullable(false);
+            $table->string('cpf', 15)->nullable(false)->unique(true);
+            $table->string('email', 80)->nullable(false);
+            $table->string('password', 255)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
